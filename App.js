8db9951a090d22d83e7bf0f9f5881b11bbb224ba
unknown <backinmarc@gmail.com>
2022-05-14 19:17:36 +0200

    drumm_Pad first Commit

--- App.js
+++ App.js
@@ -16,19 +16,19 @@ const PintaPng = (props) => {
     </View>
   );
 }
-// comentario master branch
-// Segundo comentario de master
-const ANIMALS = [
-  {name: 'Gallina', key:'1', image:<PintaPng name="gallina.jpg" />, audio:require('./assets/chickenCut.mp3') },
-  {name: 'Gat', key:'2', image:<PintaPng name="Cat.jpg" />, audio: require("./assets/cattyCut.mp3") },
-  {name: 'Gos', key:'3', image:<PintaPng name="dog.jpg" />, audio: require("./assets/dogBarkingCut.mp3")},
-  {name: 'Cavall', key:'4', image:<PintaPng name="horse.jpg" />, audio: require("./assets/horseCut.mp3")},
-  {name: 'Oca', key:'5', image:<PintaPng name="goose.jpg" />, audio: require("./assets/gooseCut.mp3")},
-  {name: 'Gall', key:'6', image:<PintaPng name="rooster.jpg" />, audio: require("./assets/roosterCut.mp3")},
-  {name: 'Bou', key:'7', image:<PintaPng name="bull.jpg" />, audio: require("./assets/bullCut.mp3")},
-  {name: 'Vaca', key:'8', image:<PintaPng name="cow.webp" />, audio: require("./assets/cowCut.mp3")},
-  {name: 'Cabra', key:'9', image:<PintaPng name="goat.jpg" />, audio: require("./assets/goatCut.mp3")},
-  {name: 'Be', key:'10', image:<PintaPng name="lamb.jpg" />, audio: require("./assets/lambCut.mp3")},
+
+const COLORS1 = [
+  {name: 'Gallina', key:'1', image:<PintaPng name="purple.png" />, audio:require('./assets/sound00.wav') },
+  {name: 'Gat', key:'2', image:<PintaPng name="red.png" />, audio: require("./assets/sound5.wav") },
+  {name: 'Gos', key:'3', image:<PintaPng name="green.png" />, audio: require("./assets/sound6.wav")},
+
+]
+
+const COLORS2 = [
+  {name: 'Gallina', key:'1', image:<PintaPng name="yellow.png" />, audio:require('./assets/sound3.wav') },
+  {name: 'Gat', key:'2', image:<PintaPng name="red.png" />, audio: require("./assets/sound4.wav") },
+  {name: 'Gos', key:'3', image:<PintaPng name="green.png" />, audio: require("./assets/sound5.wav")},
+
 ]
 // es como el main en java 
 export default function App() {
@@ -51,10 +51,10 @@ export default function App() {
     <View style={styles.container}>
        
     <FlatList
-      data={ANIMALS}
+      data={COLORS1}
       //keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) =>
-              <Text style={styles.item} onPress={()=>{playSound(item.audio)}} >{item.key} {item.name} {item.image} </Text>
+              <Text style={styles.container} onPress={()=>{playSound(item.audio)}} >{item.image} </Text>
                 }
     />
   </View>
@@ -67,14 +67,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     paddingTop: 40,
     paddingHorizontal: 20
    },
    item: {
     marginTop: 20,
     padding: 30,
-    backgroundColor: '#ffc600',
+    backgroundColor: '#black',
     fontSize: 24
    }
 });
